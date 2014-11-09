a36ad50377493165c9ed5d4d28300cd58c4d692f
Akshay S Dinesh <asdofindia@gmail.com>
2014-11-09 10:01:20 +0530

    mobile friendly v0.1.2

--- src/files/main.js
+++ src/files/main.js
@@ -9,10 +9,26 @@ function doSearch(e){
 	return false;
 }
 
+function toggleAbout(e){
+	if (e.preventDefault) e.preventDefault();
+	abouttext.style.visibility = abouttext.style.visibility=="hidden"? "visible":"hidden";
+	return false;
+}
+
 var form = document.getElementById('search');
 if (form.attachEvent) {
     form.attachEvent("submit", doSearch);
 } else {
     form.addEventListener("submit", doSearch);
+}
+
+var abouttext=document.getElementById("abouttext");
+abouttext.style.visibility="hidden";
+
+var aboutbutton = document.getElementById('aboutbutton');
+if (aboutbutton.attachEvent) {
+    aboutbutton.attachEvent("click", toggleAbout);
+} else {
+    aboutbutton.addEventListener("click", toggleAbout);
 }
 

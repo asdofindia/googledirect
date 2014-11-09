c412ccea4d599c9773906e056bbf97f99304a18a
Akshay S Dinesh <asdofindia@gmail.com>
2014-11-09 09:24:20 +0530

    mobile friendly v0.1.2

--- src/files/main.js
+++ src/files/main.js
@@ -5,7 +5,7 @@
 function doSearch(e){
 	if (e.preventDefault) e.preventDefault();
 	var query = document.getElementById('query').value;
-	window.location.assign("https://www.google.com/#q="+query+" -inurl:(htm|html|php|pls|txt) intitle:index.of \"last modified\" (mkv|mp4|avi|epub|pdf|mp3)");
+	window.location.assign("https://www.google.com/search?q="+query+" -inurl:(htm|html|php|pls|txt) intitle:index.of \"last modified\" (mkv|mp4|avi|epub|pdf|mp3)");
 	return false;
 }
 
